992eab9bfc5c5332667799fb90f510937241a7f4
mimi <mihyanggg@kakao.com>
2023-11-18 21:04:52 +0900

    Created a login and signup page using bootstrap

--- ANONYMOUS/anonymous/urls.py
+++ ANONYMOUS/anonymous/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from board.views import board
+from user.views import signin, signup
 
 urlpatterns = [
     # path('url', function), # ''로 들어오면, function을 실행시켜 줘
-    path("", board, name="board"), # 아무것도 없는 주소로 들어오면 ? board라는 함수를 실행시켜. 이름은 board로 지정 ?
+    path("board/board", board, name="board"), # 아무것도 없는 주소로 들어오면 ? board라는 함수를 실행시켜. 이름은 board로 지정 ?
+    path("user/signin", signin, name="signin"),
+    path("", signup, name="signup"),
 ]
--- ANONYMOUS/user/views.py
+++ ANONYMOUS/user/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def signin(request):
+    if request.method == "GET" :
+        return render(request, 'page/signin.html')
+
+def signup(request):
+    if request.method == "GET" :
+        return render(request, 'page/signup.html')
